38e43b5f37ac0cf70c95c5212a3741e0404db33b
Rafal <piecyk.rafal@gmial.com>
2021-05-29 13:00:44 +0200

    Login Register Log out

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.main')
+
+@section('content')
+
+<h1>Hello from index</h1>
+@endsection
---
+++ src/resources/views/templates/main.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{csrf_token()}}">
+
+        <title>{{config('app.name', 'StarWarsAPI')}}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+        <!-- Styles -->
+        <script src="{{asset('js/app.js')}}" defer></script>
+    </head>
+    <body class="antialiased">
+        <nav class="navbar navbar-expand-lg ">
+            <div class="container">
+
+                    <a class="navbar-brand" href="#">{{config('app.name', 'StarWarsAPI')}}</a>
+                    {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button> --}}
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Users</a>
+                        </li>
+                        </ul>
+                        <div class="d-flex">
+                            @if (Route::has('login'))
+                            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                @auth
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                @else
+                                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                                    @endif
+                                @endauth
+                            </div>
+                        @endif
+                        </div>
+                    </div>
+                </div>
+
+          </nav>
+
+        <main class="container">
+            @yield('content')
+        </main>
+    </body>
+</html>
